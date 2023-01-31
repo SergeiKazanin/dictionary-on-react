f844785a869cfeb40b787e2e7d2581fff6cd2fd3
SergeiKazanin <serega7667@gmail.com>
2023-01-31 12:57:31 +0700

    Add sound button and error

--- src/index.js
+++ src/index.js
@@ -25,34 +25,48 @@ function SerchForm(props) {
 }
 
 function Resalt(props) {
-  return (
-    <div className='results'>
-      <div className='res-info'>
-        <div className='res-eorld'>{props.world}</div>
-        <div className='res-sound'>
-          <VolumeUpIcon />
+  if (props.world.length) {
+    return (
+      <div className='results'>
+        <div className='res-info'>
+          <div className='res-world'>{props.world}</div>
+          <div className='res-sound' >
+            <VolumeUpIcon sx={{ fontSize: 40 }} onClick={() => props.onClick()} />
+          </div>
+        </div>
+        <div className='res-list'>
+          list
         </div>
       </div>
-      <div className='res-list'>
-        list
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
+function ErrorRes(props) {
+  if (props.errorTrue) {
+    return (<div className='error'>World not found</div>)
+  }
+}
+
+
+
 function Body() {
-  const url = 'https://od-api.oxforddictionaries.com/api/v2/entries/RU/';
+  const url = 'http://localhost:8080/https://od-api.oxforddictionaries.com/api/v2/entries/en-gb/';
   const appId = 'aaa8ae77';
   const appKey = '72decbb2a6dc1119afbe9368d28e9126';
 
   const [world, setWorld] = useState("");
-  const [sechWorld, setsechWorld] = useState("");
+  const [sechWorld, setSechWorld] = useState("");
+  const [error, setError] = useState(false);
+  const [ObjWorld, setObjWorld] = useState({
+    sound: [],
+  })
 
   const handleCangeText = (e) => {
     setWorld(e.target.value);
   }
 
-  const onSabmit = async (e) => {
+  const handleSabmit = async (e) => {
     e.preventDefault();
 
     if (!world.trim()) return;
@@ -60,17 +74,35 @@ function Body() {
     try {
       const resp = await fetch(`${url}${world}`, {
         headers: {
-          "app_id": appId,
-          "app_key": appKey
-        }
+          'app_id': appId,
+          'app_key': appKey
+        },
+        credentials: "include",
       });
       const respRes = await resp.json();
-      console.log(resp)
-      if (resp.os && respRes.length) {
-        setsechWorld(world);
+
+      if (resp.ok && respRes.results.length) {
+        setError(false);
+        setSechWorld(world);
+        const { results } = respRes;
+        setObjWorld(ObjWorld => ({
+          ...ObjWorld,
+          sound: results[0].lexicalEntries[0].entries[0].pronunciations
+        }));
+        console.log(results[0])
+      } else {
+        setError(true);
+        setSechWorld('');
       }
     } catch (err) {
       console.log(err);
+    }
+  }
+
+  const handleSound = () => {
+    if (ObjWorld.sound?.length) {
+      const sound = ObjWorld.sound[0].audioFile
+      new Audio(sound).play();
     }
   }
 
@@ -78,11 +110,9 @@ function Body() {
     <div className='app'>
       <div className='main'>
         <h1>Dictionary</h1>
-        <SerchForm onChange={handleCangeText} onSabmit={onSabmit} world={world} />
-
-        <div className='error'>World not found</div>
-
-        <Resalt world={sechWorld} />
+        <SerchForm onChange={handleCangeText} onSabmit={handleSabmit} world={world} />
+        <ErrorRes errorTrue={error} />
+        <Resalt world={sechWorld} onClick={handleSound} />
 
       </div>
     </div>
